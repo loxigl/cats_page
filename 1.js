996bf9dc69a4173013e42a70d3844e95f70f6c8b
loxigl <43906792+loxigl@users.noreply.github.com>
2021-12-03 04:25:24 +0300

    form

--- 1.js
+++ 1.js
@@ -3,6 +3,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     let a, b, sum;
     let res=document.getElementById("res")
+    let price1,price2,price3,price4,price5
+    price1=document.getElementById('price1');
+    price2=document.getElementById('price2');
+    price3=document.getElementById('price3');
+    price4=document.getElementById('price4');
+    price5=document.getElementById('price5');
+    price1.textContent="TEST"
     button.onclick = function () {
         a=(document.getElementById("input1").value)
         b=(document.getElementById("input2").value)
@@ -19,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         {
             res.textContent="Ты еблан?"
         }
+
 
     }
 });
